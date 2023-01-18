550d4fc6c27173e6035ae0fc6bfd1a80231bc802
Christian Canale <Christiancanale@gmx.de>
2023-01-18 20:42:00 +0100

    Update AuroraServices to 1.1.1
    Update AuroraServices to 1.1.1

---
+++ AuroraServices/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := permissions_com.aurora.services.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := AuroraServices
+LOCAL_SRC_FILES := AuroraServices_v1.1.1.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
+LOCAL_REQUIRED_MODULES := permissions_com.aurora.services.xml
+include $(BUILD_PREBUILT)
+
