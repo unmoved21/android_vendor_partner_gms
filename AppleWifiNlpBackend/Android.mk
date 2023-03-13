3a27697fb9a1d0812319b21479b5b5d18c9bd477
Christian Canale <Christiancanale@gmx.de>
2023-03-13 18:49:17 +0100

    Update Android.mk

--- AppleWifiNlpBackend/Android.mk
+++ AppleWifiNlpBackend/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := IAppleWifiNlpBackend
+LOCAL_MODULE := AppleWifiNlpBackend
 LOCAL_SRC_FILES := AppleWifiNlpBackend.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
