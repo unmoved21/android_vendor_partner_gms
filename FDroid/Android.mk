cdd6a0e448848345153034da87a268f580727934
Christian Canale <Christiancanale@gmx.de>
2023-03-22 17:17:06 +0100

    FDroid: Declare optional libraries w/o risk of breaking pre-19.1 builds
    This change fixes the related build error and does not take any risks regarding pre-19.1 builds similar to Use the math_gt_or_eq conditional to fix pre-19.1 builds.
    https://github.com/lineageos4microg/android_vendor_partner_gms/commit/0ec7fb7bccae82cb66582ceb14c59c3620fb309f

--- FDroid/Android.mk
+++ FDroid/Android.mk
@@ -4,6 +4,10 @@ LOCAL_MODULE := FDroid
 LOCAL_SRC_FILES := FDroid.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+# these lines may break builds before 19.1 so make them conditional
+ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+endif
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_PRODUCT_MODULE := true
 include $(BUILD_PREBUILT)
