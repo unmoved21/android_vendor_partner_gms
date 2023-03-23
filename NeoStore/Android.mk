4bb6cc4df30253954f0c5a4642908fd741b12a5a
Christian Canale <Christiancanale@gmx.de>
2023-03-23 09:01:45 +0100

    Update Android.mk

--- NeoStore/Android.mk
+++ NeoStore/Android.mk
@@ -1,13 +1,1 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE := NeoStore
-LOCAL_SRC_FILES := NeoStore.apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-# these lines may break builds before 19.1 so make them conditional
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 31),)
-LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
-endif
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_PRODUCT_MODULE := true
-include $(BUILD_PREBUILT)
+
